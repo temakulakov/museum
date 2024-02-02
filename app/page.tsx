50683f973e790716ee21007e7fe33af3e894deed
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2024-02-02 10:32:21 +0300

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 'use client'
-import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/header";
 import BottomHeader from "@/components/bottomHeader";
-import { useRouter } from 'next/router';
-import {router} from "next/client";
 import {RecoilRoot, useRecoilState} from "recoil";
-import {pageAtom} from "@/store/atoms";
 import React from "react";
 import AnimatedMount from "@/components/AnimatedMount";
 
